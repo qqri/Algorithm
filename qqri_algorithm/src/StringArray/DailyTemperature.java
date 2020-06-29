c0e574d02f2646407227c5134b261cf5c8517238
qqri <50269335+qqri@users.noreply.github.com>
2020-06-29 14:54:14 +0900

    PROGRAMMERS - DailyTemperature2

--- qqri_algorithm/src/StringArray/DailyTemperature.java
+++ qqri_algorithm/src/StringArray/DailyTemperature.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class DailyTemperature {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -12,6 +13,14 @@ public class DailyTemperature {
         int[] A = solve1(T);
         for(int i=0 ; i < T.length; i++) {
             System.out.println( A[i] );
+        }
+
+        //2
+        int[] nums = {73,74,75, 71,69,72, 76, 73};
+        int[] res = dailyTemperatures(nums);
+        System.out.println("========2.result========");
+        for(int i : res) {
+            System.out.print(i + " ");
         }
     }
 
@@ -33,6 +42,25 @@ public class DailyTemperature {
         }
 
         return A;
+    }
+
+    public static int[] dailyTemperatures(int[] temper ) {
+        //1 - stack에 넣어서 인덱스들을 비교하면서 넣는다.
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temper.length];
+
+        //2
+        for(int i = 0 ; i < temper.length ; i++ ) {
+            while(!stack.isEmpty() && temper[stack.peek()] < temper[i]) {
+                // result[0] = 1 - 0
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+
+        }
+        return result;
     }
 
 }
