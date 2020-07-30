ff3234d04f876241fce1e02c3668735f6702d192
qqri <50269335+qqri@users.noreply.github.com>
2020-07-30 16:18:11 +0900

    PROGRAMMERS - Tower

---
+++ qqri_algorithm/src/QueueNStack/Tower.java
@@ -0,0 +1,50 @@
+package QueueNStack;
+
+import java.util.Stack;
+public class Tower {
+
+}
+//프로그래머스 : https://programmers.co.kr/learn/courses/30/lessons/42588/solution_groups?language=java
+// 풀이 1
+class T_Sol {
+    class Tower {
+        int idx;
+        int height;
+
+        public Tower(int idx, int height) {
+            this.idx = idx;
+            this.height = height;
+        }
+
+        @Override
+        public String toString() {
+            return "idx : " + idx + " height : " + height;
+        }
+    }
+
+    public int[] solution(int[] heights) {
+        Stack<Tower> st = new Stack<>();
+        int[] answer = new int[heights.length];
+
+        for (int i = 0; i < heights.length; i++) {
+            Tower tower = new Tower(i + 1, heights[i]);
+            int receiveIdx = 0;
+
+            while (!st.isEmpty()) {
+                Tower top = st.peek();
+
+                if (top.height > tower.height) {
+                    receiveIdx = top.idx;
+                    break;
+                }
+
+                st.pop();
+            }
+
+            st.push(tower);
+            answer[i] = receiveIdx;
+        }
+
+        return answer;
+    }
+}
