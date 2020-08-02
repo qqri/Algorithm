8bd553a7358436fd7fd9d6a4eaf486bfd7734a2b
qqri <50269335+qqri@users.noreply.github.com>
2020-08-02 15:33:39 +0900

    PROGRAMMERS - DevelopFunc2

--- qqri_algorithm/src/QueueNStack/DevelopFunc.java
+++ qqri_algorithm/src/QueueNStack/DevelopFunc.java
@@ -10,46 +10,87 @@ public class DevelopFunc {
 
     }
 }
-
-class Solution {
+class Solution1 {
     public int[] solution(int[] progresses, int[] speeds) {
         //1
         Stack<Integer> st = new Stack<>();
-        Map<Integer, Integer> map = new HashMap();
-
+        int[] day = new int[progresses.length];
         //2
-        for(int i =0 ; i < speeds.length ; i++ ) {
-            int deploy =  ((100-progresses[i])/speeds[i]);
+        for (int i = 0; i < progresses.length; i++) {
+            int deploy = (100 - progresses[i]) / speeds[i];
 
-            if( ((100-progresses[i])%speeds[i]) != 0 ) {
+            if (((100 - progresses[i]) % speeds[i]) != 0) {
                 deploy++;
             }
-            if(i == 0 ){
-                st.push(deploy);
-                continue;
-            }
-
-            if(st.peek() <  deploy) {
-                st.push( deploy ); //남은 날짜 되는 기간
-            }
-            else{
-                st.push(st.peek());
-            }
-
+            day[i] = deploy;
         }
-
-        while(!st.isEmpty()) {
-            int cur = st.pop();
-            map.put(cur, map.getOrDefault(cur,0)+1 );
+        int cnt = 1;
+        int max = day[0];
+        st.push(1);
+        /*
+            5 5 5 // 21 25 20
+            95 95 95
+            5 4 5
+        */
+        for (int i = 1; i < day.length; i++) {
+            if (day[i] <= max) {
+                cnt++;
+                st.pop();
+                st.push(cnt);
+            } else {
+                cnt = 1;
+                st.push(cnt);
+                max = day[i];
+            }
         }
-
-        Iterator<Integer> iter = map.keySet().iterator();
-
-        int[] answer = new int[map.size()];
-        for(int i=0; i < map.size() ; i++ ) {
-            answer[i] = map.get(iter.next());
+        int size = st.size();
+        int[] answer = new int[st.size()];
+        for (int i = size - 1; i >= 0; i--) {
+            answer[i] = st.pop();
         }
         return answer;
 
+    }
+
+    class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            //1
+            Stack<Integer> st = new Stack<>();
+            Map<Integer, Integer> map = new HashMap();
+
+            //2
+            for (int i = 0; i < speeds.length; i++) {
+                int deploy = ((100 - progresses[i]) / speeds[i]);
+
+                if (((100 - progresses[i]) % speeds[i]) != 0) {
+                    deploy++;
+                }
+                if (i == 0) {
+                    st.push(deploy);
+                    continue;
+                }
+
+                if (st.peek() < deploy) {
+                    st.push(deploy); //남은 날짜 되는 기간
+                } else {
+                    st.push(st.peek());
+                }
+
+            }
+
+            while (!st.isEmpty()) {
+                int cur = st.pop();
+                map.put(cur, map.getOrDefault(cur, 0) + 1);
+            }
+
+            Iterator<Integer> iter = map.keySet().iterator();
+
+            int[] answer = new int[map.size()];
+            for (int i = 0; i < map.size(); i++) {
+                answer[i] = map.get(iter.next());
+            }
+            return answer;
+
+        }
     }
 }
