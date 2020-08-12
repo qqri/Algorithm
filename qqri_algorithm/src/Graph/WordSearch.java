511e5ae8d290b26cc22efa1556f5e0d50bb0ea8e
qqri <50269335+qqri@users.noreply.github.com>
2020-08-12 11:18:54 +0900

    PROGRAMMERS - WordSearch2

--- qqri_algorithm/src/Graph/WordSearch.java
+++ qqri_algorithm/src/Graph/WordSearch.java
@@ -11,11 +11,62 @@ public class WordSearch {
                             {'S','F','C','S'},
                             {'A','D','E','E'}   };
 
-        String str = "ABCB";
+        String str = "SEE";
 
 
-        System.out.println(sol1(board, str) );
+        System.out.println(solution(board, str) );
     }
+
+    public static boolean solution(char[][] board , String str) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if(m == 0 || n == 0 || board==null || str.length() == 0) return false;
+
+        int[][] visited = new int[m][n];
+
+        for(int i = 0 ; i < m ; i++ ) {
+            for(int j = 0 ; j < n  ; j++ ) {
+                if(dfs(board, visited, i, j, 0, str)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static int[][] dirs = {{1,0} , {-1,0} , {0,1}, {0,-1}};
+
+    public static boolean dfs( char[][] board, int[][] visited, int i, int j, int index, String str ){
+        int m = board.length;
+        int n = board[0].length;
+        // 기저 사례
+        if(index == str.length()) return true;
+
+        if(i < 0 || i >= m || j < 0 || j >= n||board == null || visited == null || str.length() == 0) return false;
+        if(visited[i][j] == 1) return false; //한번 왔다 간 곳
+        if( board[i][j] != str.charAt(index) ) return false;
+
+
+        visited[i][j] = 1; //방문했으니 변경해줌.
+
+        for(int[] dir : dirs) {
+            int nextX = i + dir[0];
+            int nextY = j + dir[1];
+            if( dfs(board, visited, nextX, nextY, index+1, str) ) return true;
+        }
+
+        visited[i][j] = 0;
+        return false;
+    }
+
+
+
+
+    // =============================== //
+
+
 
     public static boolean sol1(char[][] board, String str) {
         ArrayList<ArrayList<Integer>> firstChar  = new ArrayList<>();
@@ -42,8 +93,8 @@ public class WordSearch {
         return false;
     }
 
-    static int[] dx = {1, 1, 1, 0, 0, -1, -1, -1};
-    static int[] dy = {1, 0, -1, 1, -1, 1, 0, -1};
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, 1, -1, 0};
 
     public static boolean sol2(char[][] board, int x, int y, String str) {
 
@@ -61,7 +112,7 @@ public class WordSearch {
 
         board[x][y] = '0'; //한번 갔다 온 곳은 표시해둠.
 
-        for(int i = 0 ; i < 8 ; i++ ) {
+        for(int i = 0 ; i < 4 ; i++ ) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
             if(sol2(board, nextX, nextY, str.substring(1)))
