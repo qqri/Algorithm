95bef77834e2a3277933c6fd6be027fdb3d7bd0c
qqri <50269335+qqri@users.noreply.github.com>
2021-03-28 22:14:50 +0900

    reTry

--- qqri_algorithm/src/DivideAndConquer/Hanoi.java
+++ qqri_algorithm/src/DivideAndConquer/Hanoi.java
@@ -1,3 +1,5 @@
+package DivideAndConquer;
+
 import java.util.Scanner;
 import java.lang.*;
 
--- qqri_algorithm/src/Graph/CourseSch.java
+++ qqri_algorithm/src/Graph/CourseSch.java
@@ -1,6 +1,8 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class CourseSch {
@@ -45,7 +47,7 @@ public class CourseSch {
                 }
             }
         }
-
+        
         for(int i = 0 ; i < inDegreeLength ; i++ ) {
             if(inDegree[i] != 0) {
                 return false;
